9f2a6eeb85cdb701242176ddf319c69bee628884
David <kooldave98@hotmail.com>
2016-06-27 22:07:25 +0100

    Initial commit

---
+++ StackView/BaseViewController.cs
@@ -0,0 +1,140 @@
+﻿#if __IOS__
+using System;
+using UIKit;
+
+namespace common_lib
+{
+    public abstract class SimpleViewController : UIViewController
+    {
+        protected bool have_constraints_been_added { get; set; }
+
+        public override void LoadView ()
+        {
+            base.LoadView ();
+
+            have_constraints_been_added = false;
+            ApplyConstraints ();
+
+
+            View.BackgroundColor = UIColor.White;
+
+            WillPopulateView ();
+            WillDatabindView ();
+        }
+
+        public override void UpdateViewConstraints ()
+        {
+            if (!have_constraints_been_added) {
+                WillAddConstraints ();
+
+                have_constraints_been_added = true;
+            } else {
+                WillUpdateConstraints ();
+            }
+
+            base.UpdateViewConstraints ();
+        }
+
+        /// <summary>
+        /// Sounds like a bad name for this method,
+        /// dont like that it sounds right with a bool passed in,
+        /// but off mark without the bool
+        /// </summary>
+        /// <param name="right_now">If set to <c>true</c> right now.</param>
+        public void ApplyConstraints (bool right_now = false)
+        {
+            View.SetNeedsUpdateConstraints ();
+            if (right_now) {
+                View.UpdateConstraintsIfNeeded ();
+            }
+        }
+
+        public virtual void WillPopulateView () { }
+
+        public virtual void WillDatabindView () { }
+
+        public virtual void WillAddConstraints () { }
+
+        public virtual void WillUpdateConstraints () { }
+
+        //        private static CGSize textSize = new NSString("The quick brown fox jumped over the lazy dog" +
+        //                                        "The quick brown fox jumped over the lazy dog" +
+        //                                        "The quick brown fox jumped over the lazy dog" +
+        //                                        "The quick brown fox jumped over the lazy dog")
+        //            .StringSize(UIFont.SystemFontOfSize(12f));
+        //
+        //        protected nfloat one_sixty_character_size = textSize.Height;
+
+        protected nfloat parent_child_margin = HumanInterface.parent_child_margin;
+
+        protected nfloat double_parent_child_margin = HumanInterface.parent_child_margin * 2;
+
+        protected nfloat sibling_sibling_margin = HumanInterface.sibling_sibling_margin;
+
+        protected nfloat combined_parent_and_sibling_margin = HumanInterface.sibling_sibling_margin + HumanInterface.parent_child_margin;
+
+        protected nfloat double_sibling_sibling_margin = HumanInterface.sibling_sibling_margin * 2;
+
+        protected nfloat finger_tip_diameter = HumanInterface.finger_tip_diameter;
+
+        protected nfloat double_finger_tip_diameter = HumanInterface.finger_tip_diameter * 2;
+
+        protected void hide_keyboard_for ()
+        {
+            //hide keyboard for a soft keyboard    
+        }
+
+        protected void show_progress (string title = "Loading...", string message = "Busy")
+        {
+            UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
+            //BTProgressHUD.Show (title);
+        }
+
+        protected void dismiss_progress ()
+        {
+            UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+            //BTProgressHUD.Dismiss ();
+        }
+
+        protected void show_toast (string message)
+        {
+            //BTProgressHUD.ShowToast (message, false, 5000);
+        }
+
+        protected void show_alert (string title, string message, string ok_button_text = "Ok", Action ok_button_action = null)
+        {
+            var alert = new UIAlertView (title, message, null, ok_button_text);
+
+            alert.Clicked += delegate {
+                if (ok_button_action != null) {
+                    ok_button_action ();
+                }
+            };
+
+            alert.Show ();
+        }
+
+        protected void notify (String methodName)
+        {
+            //lock screen notifications
+        }
+
+        private static bool UserInterfaceIdiomIsPhone {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+    }
+
+    public static class HumanInterface
+    {
+        //http://stackoverflow.com/questions/14055900/what-constant-can-i-use-for-the-default-aqua-space-in-autolayout
+        public static nfloat parent_child_margin = 20.0f;
+        public static nfloat sibling_sibling_margin = 8.0f;
+
+        public static nfloat finger_tip_diameter = 30.0f;
+
+        public static nfloat thumb_tip_diameter = 50.0f;
+
+
+    }
+}
+#endif
---
+++ StackView/ViewController.cs
@@ -0,0 +1,113 @@
+﻿using common_lib;
+using CoreGraphics;
+using UIKit;
+
+namespace StackView
+{
+    public class ViewController : SimpleViewController
+    {
+        UIScrollView ScrollView { get; set; }
+        UIStackView stackView { get; set; }
+
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            apply_simple_border (View);
+
+            View.AddSubview (ScrollView = new UIScrollView ());
+            ScrollView.TranslatesAutoresizingMaskIntoConstraints = false;
+            apply_simple_border (ScrollView);
+            ScrollView.BackgroundColor = UIColor.Purple;
+
+
+            ScrollView.AddSubview (stackView = new UIStackView ());
+
+            stackView.TranslatesAutoresizingMaskIntoConstraints = false;
+            //RatingView.Axis = UILayoutConstraintAxis.Vertical;
+            stackView.Alignment = UIStackViewAlignment.Bottom;
+            stackView.Distribution = UIStackViewDistribution.Fill;
+            //apply_simple_border (stackView, UIColor.Purple.CGColor);
+            stackView.BackgroundColor = UIColor.Orange;
+
+
+        }
+
+        public override void ViewWillAppear (bool animated)
+        {
+            base.ViewWillAppear (animated);
+
+
+            for (int i = 0; i < 56; i++) {
+
+
+                IncreaseRating ();
+            }
+
+
+            //RatingView.LayoutIfNeeded ();
+
+        }
+
+        public override void WillAddConstraints ()
+        {
+            View.ConstrainLayout (() =>
+                                  ScrollView.Frame.Top == View.Bounds.Top + sibling_sibling_margin &&
+                                  ScrollView.Frame.Left == View.Bounds.Left + sibling_sibling_margin &&
+                                  ScrollView.Frame.Right == View.Bounds.Right - sibling_sibling_margin &&
+                                  ScrollView.Frame.Bottom == View.Bounds.Bottom - sibling_sibling_margin
+                                 );
+
+            ScrollView.ConstrainLayout (() =>
+                                        stackView.Frame.Top == ScrollView.Bounds.Top + sibling_sibling_margin &&
+                                        stackView.Frame.Left == ScrollView.Bounds.Left + sibling_sibling_margin &&
+                                        stackView.Frame.Right == ScrollView.Bounds.Right - sibling_sibling_margin &&
+                                        stackView.Frame.Bottom == ScrollView.Bounds.Bottom - sibling_sibling_margin
+                                 );
+        }
+
+        void IncreaseRating ()//bool animate = false)
+        {
+
+            // Create new rating icon and add it to stack
+            var icon = new UIImageView (new UIImage ("icon.png"));
+            icon.TranslatesAutoresizingMaskIntoConstraints = false;
+            icon.ContentMode = UIViewContentMode.ScaleAspectFit;
+            stackView.AddArrangedSubview (icon);
+
+            apply_simple_border (icon);
+
+            // Animate stack
+            //UIView.Animate (0.25, () => {
+            //    // Adjust stack view
+            //    RatingView.LayoutIfNeeded ();
+            //});
+
+        }
+
+        void DecreaseRating ()
+        {
+
+            // Get the last subview added
+            var icon = stackView.ArrangedSubviews [stackView.ArrangedSubviews.Length - 1];
+
+            // Remove from stack and screen
+            stackView.RemoveArrangedSubview (icon);
+            icon.RemoveFromSuperview ();
+
+            // Animate stack
+            UIView.Animate (0.25, () => {
+                // Adjust stack view
+                stackView.LayoutIfNeeded ();
+            });
+        }
+
+        public static void apply_simple_border (UIView view, CGColor color = null)
+        {
+            view.Layer.BorderWidth = 1;
+            view.Layer.BorderColor = color ?? UIColor.Black.CGColor;
+        }
+    }
+}
+
